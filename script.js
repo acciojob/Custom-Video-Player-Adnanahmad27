3e617d3e08692dcd5231987e3c117f3b585ee1c0
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-03-03 03:55:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,38 +4,35 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]'); 
 const ranges = player.querySelectorAll('.player__slider');
-const volume = ranges[0];
-const speed = ranges[1]; 
-const backwardBtn = skipButtons[0];
-const forwardBtn = skipButtons[1];
 function changeSeekTime(dir){
     dir==='forward' ? (video.currentTime += 25) : (video.currentTime -= 10);
 }
 
+toggle.addEventListener('click', ()=>{
+    if(video.paused){
+        toggle.innerText = '❚ ❚';
+        video.play();
+    }else{
+        video.pause();
+        toggle.innerText = '►';
+        
+    }
+});
 video.addEventListener('timeupdate' , ()=>{
     let widthInPercentage = (video.currentTime * 100)/video.duration;
     progressBar.style.width = `${widthInPercentage}%`;
 });
 
-forwardBtn.addEventListener('click',()=>changeSeekTime('forward'));
-backwardBtn.addEventListener('click', ()=>changeSeekTime('backward'));
+skipButtons[1].addEventListener('click',()=>changeSeekTime('forward'));
+skipButtons[0].addEventListener('click', ()=>changeSeekTime('backward'));
 
-volume.addEventListener('change' ,()=>{
-    let selectedValue = volume.value;
+ranges[0].addEventListener('change' ,()=>{
+    let selectedValue = ranges[0].value;
     video.volume = selectedValue
 });
-speed.addEventListener('change',()=>{
-    let selectedValue = speed.value;
-    video.playbackRate = speedMap[selectedValue];
+ranges[1].addEventListener('change',()=>{
+    let selectedValue = ranges[1].value;
+    video.playbackRate = selectedValue;
 });
-toggle.addEventListener('click' , () => {
-	if(video.paused){
-		toggle.innerText = '❚ ❚'; 
-		video.play();
-	}else{
-		video.pause();
-		toggle.innerText = '► '
-	}
-})
